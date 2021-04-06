f4974301e14e847645f9f84a39395a18992da835
leha-flex <neimuro@yandex.ru>
2021-04-07 00:57:38 +0300

    lab2-3 in progress

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -2,6 +2,14 @@
 #include <string>
 #include <android/log.h>
 
+#include "C:\Users\DEDLXCK\AndroidStudioProjects\libs\mbedtls\mbedtls\include\mbedtls/entropy.h"
+#include "C:\Users\DEDLXCK\AndroidStudioProjects\libs\mbedtls\mbedtls\include\mbedtls\ctr_drbg.h"
+#include <C:\Users\DEDLXCK\AndroidStudioProjects\libs\mbedtls\mbedtls\include\mbedtls\des.h>
+
+mbedtls_entropy_context entropy;
+mbedtls_ctr_drbg_context ctr_drbg;
+char *personalization = "fclient-sample-app";
+
 #define LOG_INFO(...) __android_log_print(ANDROID_LOG_INFO, "chel_ndk", __VA_ARGS__)
 
 #include <C:\Users\DEDLXCK\AndroidStudioProjects\libs\spdlog\include\spdlog\spdlog.h>
@@ -19,3 +27,57 @@ Java_com_pro_rpolabs_MainActivity_stringFromJNI(
     SLOG_INFO("Hello from spdlog {}", 2021);
     return env->NewStringUTF(hello.c_str());
 }
+/* extern "C" JNIEXPORT jint JNICALL
+Java_ru_iu3_fclient_MainActivity_initRng(JNIEnv *env, jclass clazz) {
+    mbedtls_entropy_init( &entropy );
+    mbedtls_ctr_drbg_init( &ctr_drbg );
+
+    return mbedtls_ctr_drbg_seed( &ctr_drbg , mbedtls_entropy_func, &entropy,
+                                  (const unsigned char *) personalization,
+                                  strlen( personalization ) );
+}
+extern "C" JNIEXPORT jbyteArray JNICALL
+Java_ru_iu3_fclient_MainActivity_randomBytes(JNIEnv *env, jclass, jint no) {
+    uint8_t * buf = new uint8_t [no];
+    mbedtls_ctr_drbg_random(&ctr_drbg, buf, no);
+    jbyteArray rnd = env->NewByteArray(no);
+    env->SetByteArrayRegion(rnd, 0, no, (jbyte *)buf);
+    delete[] buf;
+    return rnd;
+}
+
+// https://tls.mbed.org/api/des_8h.html
+extern "C" JNIEXPORT jbyteArray JNICALL
+Java_ru_iu3_fclient_MainActivity_encrypt(JNIEnv *env, jclass, jbyteArray key, jbyteArray data)
+{
+
+    jsize ksz = env->GetArrayLength(key);
+    jsize dsz = env->GetArrayLength(data);
+    if ((ksz != 16) || (dsz <= 0)) {
+        return env->NewByteArray(0);
+    }
+    mbedtls_des3_context ctx;
+    mbedtls_des3_init(&ctx);
+
+    jbyte * pkey = env->GetByteArrayElements(key, 0);
+
+    // PKCS#5 padding
+    int rst = dsz % 8;
+    int sz = dsz + 8 - rst;
+    uint8_t * buf = new uint8_t[sz];
+    for (int i = 7; i > rst; i--)
+        buf[dsz + i] = rst;
+    jbyte * pdata = env->GetByteArrayElements(data, 0);
+    std::copy(pdata, pdata + dsz, buf);
+    mbedtls_des3_set2key_enc(&ctx, (uint8_t *)pkey);
+    int cn = sz / 8;
+    for (int i = 0; i < cn; i++)
+        mbedtls_des3_crypt_ecb(&ctx, buf + i*8, buf + i*8);
+    jbyteArray dout = env->NewByteArray(sz);
+    env->SetByteArrayRegion(dout, 0, sz, (jbyte *)buf);
+    delete[] buf;
+    env->ReleaseByteArrayElements(key, pkey, 0);
+    env->ReleaseByteArrayElements(data, pdata, 0);
+    return dout;
+} */
+
