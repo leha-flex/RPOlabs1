e0e63327a83e30ff00573d80215be7188d690dca
leha-flex <neimuro@yandex.ru>
2021-06-13 23:06:48 +0300

    http is done

--- app/src/main/java/com/pro/rpolabs/MainActivity.java
+++ app/src/main/java/com/pro/rpolabs/MainActivity.java
@@ -1,29 +1,20 @@
 package com.pro.rpolabs;
-import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
 import android.widget.Toast;
-
-import java.io.InputStream;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.nio.charset.StandardCharsets;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import org.apache.commons.io.IOUtils;
+import androidx.appcompat.app.AppCompatActivity;
 
 import org.apache.commons.codec.DecoderException;
 import org.apache.commons.codec.binary.Hex;
 
-
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
-import android.widget.Toast;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -43,6 +34,11 @@ public class MainActivity extends AppCompatActivity {
         Button btn = findViewById(R.id.btnClick);
         btn.setOnClickListener((View v) -> { onButtonClick(v);});
 
+        Button btnHttp = findViewById(R.id.btnHttp);
+        btnHttp.setOnClickListener((View v) -> {
+            onButtonHttpClick(v);
+        });
+
         int res = initRng();
         Log.i("rpolabs", "Init Rng = " + res);
         byte[] v = randomBytes(10);
@@ -58,9 +54,42 @@ public class MainActivity extends AppCompatActivity {
         return new byte[0];
     }
     protected void onButtonClick(View v) {
-       // Toast.makeText(this, "clicked!", Toast.LENGTH_SHORT).show();
+        Log.e("Button click", "clicked");
+        Toast.makeText(this, "clicked!", Toast.LENGTH_SHORT).show();
         Intent it = new Intent(this, PinpadActivity.class);
         startActivity(it);
+    }
+    protected void onButtonHttpClick(View v) {
+        TestHttpClient();
+    }
+
+    protected void TestHttpClient() {
+        new Thread(() -> {
+            try {
+                HttpURLConnection uc = (HttpURLConnection) (new URL("https://www.wikipedia.org").openConnection());
+                //HttpURLConnection uc = (HttpURLConnection) (new URL("http://10.0.2.2:8081/api/v1/title").openConnection());
+                InputStream inputStream = uc.getInputStream();
+                String html = IOUtils.toString(inputStream);
+                String title = getPageTitle(html);
+                runOnUiThread(() -> {
+                    Log.e("Button click", "http clicked");
+                    Toast.makeText(this, title, Toast.LENGTH_SHORT).show();
+                });
+            } catch (Exception ex) {
+                Log.e("fapptag", "Http client fails", ex);
+            }
+        }).start();
+    }
+
+    protected String getPageTitle(String html) {
+        int pos = html.indexOf("<title");
+        String p = "not found";
+        if (pos >= 0) {
+            int pos2 = html.indexOf("<", pos + 1);
+            if (pos >= 0)
+                p = html.substring(pos + 7, pos2);
+        }
+        return p;
     }
 
 
