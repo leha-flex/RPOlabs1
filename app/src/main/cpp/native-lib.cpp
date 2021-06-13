f1ed0e2753215272d7cc77ca8bd495750d89f7ba
leha-flex <neimuro@yandex.ru>
2021-06-13 22:16:49 +0300

    pinpad is done

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,22 +1,15 @@
 #include <jni.h>
 #include <string>
 #include <android/log.h>
-
-#include "C:\Users\DEDLXCK\AndroidStudioProjects\libs\mbedtls\mbedtls\include\mbedtls/entropy.h"
-#include "C:\Users\DEDLXCK\AndroidStudioProjects\libs\mbedtls\mbedtls\include\mbedtls\ctr_drbg.h"
-#include <C:\Users\DEDLXCK\AndroidStudioProjects\libs\mbedtls\mbedtls\include\mbedtls\des.h>
-
-mbedtls_entropy_context entropy;
-mbedtls_ctr_drbg_context ctr_drbg;
-char *personalization = "fclient-sample-app";
+#include <memory>
 
 #define LOG_INFO(...) __android_log_print(ANDROID_LOG_INFO, "chel_ndk", __VA_ARGS__)
 
-#include <C:\Users\DEDLXCK\AndroidStudioProjects\libs\spdlog\include\spdlog\spdlog.h>
-#include "C:\Users\DEDLXCK\AndroidStudioProjects\libs\spdlog\include\spdlog\sinks\android_sink.h"
+#include <spdlog/spdlog.h>
+#include "spdlog/sinks/android_sink.h"
 
 #define SLOG_INFO(...) android_logger->info( __VA_ARGS__ )
-auto android_logger = spdlog::android_logger_mt( "android", "rpo_ndk");
+auto android_logger = spdlog::android_logger_mt( "android", "chel_ndk");
 
 extern "C" JNIEXPORT jstring JNICALL
 Java_com_pro_rpolabs_MainActivity_stringFromJNI(
@@ -27,57 +20,102 @@ Java_com_pro_rpolabs_MainActivity_stringFromJNI(
     SLOG_INFO("Hello from spdlog {}", 2021);
     return env->NewStringUTF(hello.c_str());
 }
-/* extern "C" JNIEXPORT jint JNICALL
-Java_ru_iu3_fclient_MainActivity_initRng(JNIEnv *env, jclass clazz) {
-    mbedtls_entropy_init( &entropy );
-    mbedtls_ctr_drbg_init( &ctr_drbg );
 
-    return mbedtls_ctr_drbg_seed( &ctr_drbg , mbedtls_entropy_func, &entropy,
-                                  (const unsigned char *) personalization,
-                                  strlen( personalization ) );
+#include "C:\Users\DEDLXCK\AndroidStudioProjects\libs\mbedtls\mbedtls\include\mbedtls\entropy.h"
+#include "C:\Users\DEDLXCK\AndroidStudioProjects\libs\mbedtls\mbedtls\include\mbedtls/ctr_drbg.h"
+#include "C:\Users\DEDLXCK\AndroidStudioProjects\libs\mbedtls\mbedtls\include\mbedtls/des.h"
+
+mbedtls_entropy_context entropy;
+mbedtls_ctr_drbg_context ctrDrbgContext;
+std::string personalization = "chel-sample-app";
+
+extern "C" JNIEXPORT jint JNICALL
+Java_com_pro_rpolabs_MainActivity_initRng(
+        JNIEnv *env,
+        jclass clazz ) {
+    mbedtls_entropy_init(&entropy);
+    mbedtls_ctr_drbg_init(&ctrDrbgContext);
+    return mbedtls_ctr_drbg_seed(
+            &ctrDrbgContext, mbedtls_entropy_func, &entropy,
+            reinterpret_cast<const unsigned char*>(personalization.c_str()),
+            personalization.size()
+    );
 }
+
 extern "C" JNIEXPORT jbyteArray JNICALL
-Java_ru_iu3_fclient_MainActivity_randomBytes(JNIEnv *env, jclass, jint no) {
-    uint8_t * buf = new uint8_t [no];
-    mbedtls_ctr_drbg_random(&ctr_drbg, buf, no);
-    jbyteArray rnd = env->NewByteArray(no);
-    env->SetByteArrayRegion(rnd, 0, no, (jbyte *)buf);
-    delete[] buf;
+Java_com_pro_rpolabs_MainActivity_randomBytes(
+        JNIEnv *env,
+        jclass clazz,
+        jint n ) {
+    std::unique_ptr<uint8_t[]> buf = std::make_unique<uint8_t[]>(n);
+    mbedtls_ctr_drbg_random(&ctrDrbgContext, buf.get(), n);
+    jbyteArray rnd = env->NewByteArray(n);
+    env->SetByteArrayRegion(rnd, 0, n, reinterpret_cast<jbyte*>(buf.get()));
     return rnd;
 }
 
-// https://tls.mbed.org/api/des_8h.html
 extern "C" JNIEXPORT jbyteArray JNICALL
-Java_ru_iu3_fclient_MainActivity_encrypt(JNIEnv *env, jclass, jbyteArray key, jbyteArray data)
-{
-
-    jsize ksz = env->GetArrayLength(key);
-    jsize dsz = env->GetArrayLength(data);
-    if ((ksz != 16) || (dsz <= 0)) {
+Java_com_pro_rpolabs_MainActivity_encrypt(
+        JNIEnv *env,
+        jclass clazz,
+        jbyteArray key,
+        jbyteArray data ) {
+    jsize keySize = env->GetArrayLength(key);
+    jsize dataSize = env->GetArrayLength(data);
+    SLOG_INFO("Encrypt: {} {}", keySize, dataSize);
+    if(keySize != 16 || dataSize <= 0) {
         return env->NewByteArray(0);
     }
     mbedtls_des3_context ctx;
     mbedtls_des3_init(&ctx);
-
-    jbyte * pkey = env->GetByteArrayElements(key, 0);
-
-    // PKCS#5 padding
-    int rst = dsz % 8;
-    int sz = dsz + 8 - rst;
-    uint8_t * buf = new uint8_t[sz];
-    for (int i = 7; i > rst; i--)
-        buf[dsz + i] = rst;
-    jbyte * pdata = env->GetByteArrayElements(data, 0);
-    std::copy(pdata, pdata + dsz, buf);
-    mbedtls_des3_set2key_enc(&ctx, (uint8_t *)pkey);
+    jbyte *pKey = env->GetByteArrayElements(key, 0);
+    int rst = dataSize % 8;
+    int sz = dataSize + 8 - rst;
+    std::unique_ptr<uint8_t[]> buf = std::make_unique<uint8_t[]>(sz);
+    for (int i = 7; i > rst; --i) {
+        buf[dataSize + i] = rst;
+    }
+    jbyte *pData = env->GetByteArrayElements(data, 0);
+    std::copy(pData, pData + dataSize, buf.get());
+    mbedtls_des3_set2key_enc(&ctx, reinterpret_cast<uint8_t*>(pKey));
     int cn = sz / 8;
-    for (int i = 0; i < cn; i++)
-        mbedtls_des3_crypt_ecb(&ctx, buf + i*8, buf + i*8);
+    for (int i = 0; i < cn; ++i) {
+        mbedtls_des3_crypt_ecb(&ctx, buf.get() + i * 8, buf.get() + i * 8);
+    }
     jbyteArray dout = env->NewByteArray(sz);
-    env->SetByteArrayRegion(dout, 0, sz, (jbyte *)buf);
-    delete[] buf;
-    env->ReleaseByteArrayElements(key, pkey, 0);
-    env->ReleaseByteArrayElements(data, pdata, 0);
+    env->SetByteArrayRegion(dout, 0, sz, reinterpret_cast<jbyte *>(buf.get()));
+    env->ReleaseByteArrayElements(key, pKey, 0);
+    env->ReleaseByteArrayElements(data, pData, 0);
     return dout;
-} */
+}
 
+extern "C" JNIEXPORT jbyteArray JNICALL
+Java_com_pro_rpolabs_MainActivity_decrypt(
+        JNIEnv *env,
+        jclass clazz,
+        jbyteArray key,
+        jbyteArray data ) {
+    jsize keySize = env->GetArrayLength(key);
+    jsize dataSize = env->GetArrayLength(data);
+    SLOG_INFO("Decrypt: {} {}", keySize, dataSize);
+    if(keySize != 16 || dataSize <= 0 || (dataSize % 8) != 0) {
+        return env->NewByteArray(0);
+    }
+    mbedtls_des3_context ctx;
+    mbedtls_des3_init(&ctx);
+    jbyte *pKey = env->GetByteArrayElements(key, 0);
+    jbyte *pData = env->GetByteArrayElements(data, 0);
+    std::unique_ptr<uint8_t[]> buf = std::make_unique<uint8_t[]>(dataSize);
+    std::copy(pData, pData + dataSize, buf.get());
+    mbedtls_des3_set2key_dec(&ctx, reinterpret_cast<uint8_t*>(pKey));
+    int cn = dataSize / 8;
+    for (int i = 0; i < cn; ++i) {
+        mbedtls_des3_crypt_ecb(&ctx, buf.get() + i * 8, buf.get() + i * 8);
+    }
+    int sz = dataSize - 8 + buf[dataSize - 1];
+    jbyteArray dout = env->NewByteArray(cn * 8);
+    env->SetByteArrayRegion(dout, 0, cn * 8, reinterpret_cast<jbyte *>(buf.get()));
+    env->ReleaseByteArrayElements(key, pKey, 0);
+    env->ReleaseByteArrayElements(data, pData, 0);
+    return dout;
+}
--- app/src/main/java/com/pro/rpolabs/MainActivity.java
+++ app/src/main/java/com/pro/rpolabs/MainActivity.java
@@ -1,9 +1,7 @@
 package com.pro.rpolabs;
-
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-import android.widget.TextView;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -11,15 +9,30 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import org.apache.commons.codec.DecoderException;
 import org.apache.commons.codec.binary.Hex;
+
+
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     // Used to load the 'native-lib' library on application startup.
+    // Used to load the 'native-lib' library on application startup.
     static {
         System.loadLibrary("native-lib");
         System.loadLibrary("mbedcrypto");
+        initRng();
     }
 
     @Override
@@ -27,11 +40,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button btn = findViewById(R.id.btnClickMe);
+        Button btn = findViewById(R.id.btnClick);
         btn.setOnClickListener((View v) -> { onButtonClick(v);});
 
         int res = initRng();
-        Log.i("fclient", "Init Rng = " + res);
+        Log.i("rpolabs", "Init Rng = " + res);
         byte[] v = randomBytes(10);
     }
 
@@ -45,7 +58,9 @@ public class MainActivity extends AppCompatActivity {
         return new byte[0];
     }
     protected void onButtonClick(View v) {
-        Toast.makeText(this, stringFromJNI(), Toast.LENGTH_SHORT).show();
+       // Toast.makeText(this, "clicked!", Toast.LENGTH_SHORT).show();
+        Intent it = new Intent(this, PinpadActivity.class);
+        startActivity(it);
     }
 
 
